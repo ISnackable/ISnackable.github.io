75d0e813fef38fca83a04e5932069418e8313879
Tommy Teo <52971804+ISnackable@users.noreply.github.com>
2024-11-26 06:10:14 +0000

    fix: devcontainer

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   : process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
